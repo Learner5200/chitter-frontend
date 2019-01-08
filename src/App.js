0c09fa6921855dedbe70fd29c333ea09b3ce888f
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-08 15:54:38 +0000

    App renders navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Navbar from './Navbar';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 export default App;
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Navbar from './Navbar';
 
 describe('App', () => {
+  let app;
+  beforeEach(() => {
+    app = shallow(<App />);
+  });
   it('should render a <div>', () => {
-    const app = shallow(<App />);
     expect(app.find('div').length).toEqual(1);
+  });
+  it('should render a Navbar', () => {
+    expect(app.containsMatchingElement(<Navbar />)).toEqual(true);
   });
 });
